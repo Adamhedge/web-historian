a8e03985bd50fde1f0de751c4ba70dbaac6590c4
Adam Hedgpeth <Adamhedge@gmail.com>
2015-09-09 14:50:48 -0700

    I can serve the index.

--- web/basic-server.js
+++ web/basic-server.js
@@ -1,14 +1,31 @@
 var http = require("http");
 var handler = require("./request-handler");
 var initialize = require("./initialize.js");
-
+var messages = require('./request-handler');
+var url = require("url");
+var utils = require('./utils');
 // Why do you think we have this here?
 // HINT: It has to do with what's in .gitignore
 initialize("./archives");
 
-var port = 8080;
+var router = {
+  '/': messages.requestHandler
+  // ...
+};
+
+var port = 3001;
 var ip = "127.0.0.1";
-var server = http.createServer(handler.handleRequest);
+
+var server = http.createServer( function(req, res){
+  console.log("Serving request type " + req.method + " for url " + req.url);
+  messages.requestHandler(req, res);
+  var route = router[url.parse(req.url).pathname];
+  // if (route) {
+  //   route(req, res);
+  // } else {
+  //   utils.sendResponse(res, '', 404);
+  // }
+});
 
 if (module.parent) {
   module.exports = server;
@@ -16,4 +33,3 @@ if (module.parent) {
   server.listen(port, ip);
   console.log("Listening on http://" + ip + ":" + port);
 }
-
--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,71 @@
 var path = require('path');
+var utils = require('./utils');
 var archive = require('../helpers/archive-helpers');
+var fs = require("fs");
+var mime = require('mime');
+var url = require('url');
 // require more modules/folders here!
 
-exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+var headers = {
+  "access-control-allow-origin": "*",
+  "access-control-allow-methods": "GET, POST, PUT, DELETE, OPTIONS",
+  "access-control-allow-headers": "content-type, accept",
+  "access-control-max-age": 10, // Seconds.
+  'Content-Type': "application/json"
 };
+
+var serveFile = function(){
+  var fileName;
+    if(subURL === '/'){
+      fileName = path.join(__dirname, './public/index.html');
+    } else {
+      fileName = path.join(__dirname, './pulic/' + subURL);
+    }
+    fs.readFile(fileName, function(err, data){
+      if(!err){
+        headers['Content-Type'] = mime.lookup(fileName);
+        response.writeHead(200, headers);
+        response.end(data.toString());
+      } else{
+        console.log(err);
+      }
+    });
+};
+
+var actions = {
+  'GET': function(request, response) {
+    var subURL = request.url.split('?')[0];
+    var fileName;
+    //console.log(subURL);
+    if(subURL === '/'){
+      fileName = path.join(__dirname, './public/index.html');
+    } else {
+      fileName = path.join(__dirname, './public/' + subURL);
+    }
+    fs.readFile(fileName, function(err, data){
+      if(!err){
+        headers['Content-Type'] = mime.lookup(fileName);
+        response.writeHead(200, headers);
+        response.end(data.toString());
+      } else{
+        console.log(subURL)
+      }
+    });
+  },
+  'POST': function(request, response) {
+    utils.collectData(request, function(message) {
+      message.objectId = ++objectIdCounter;
+      messages.push(message);
+      utils.sendResponse(response, {objectId: message.objectId}, 201);
+    });
+  },
+  'OPTIONS': function(request, response) {
+    utils.sendResponse(response, null);
+  }
+};
+
+exports.requestHandler = utils.makeActionHandler(actions);
+
+// headers['Content-Type'] = mime.lookup(fileName);
+//         response.writeHead(200, headers);
+//         response.end(data.toString());
